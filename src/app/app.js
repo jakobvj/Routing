c5fc0cfb0ce2d8a6abcbb6ddb37ce51f16cf99fd
Jakob Jensen <jvj@eucsyd.dk>
2019-03-27 14:44:54 +0100

    Routing setup so that dunkirk is showing up on every routes

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,59 @@
 import Vue from 'vue';
 
+const routes = [
+  {
+    path: '/',
+    component: {
+      name: 'index-blurb',
+      template: `<h2>Pick a Christopher Nolan movie!</h2>`
+    }
+  },
+  {path: '/dunkirk', component: DunkirkBlurb},
+  {path: '/interstellar', component: InterstellarBlurb},
+  {path: '/the-dark-knight-rises', component: TheDarkKnightRisesBlurb}
+];
+const DunkirkBlurb = {
+  name: 'dunkirk-blurb',
+  template: `<div>
+    <h2>Dunkirk</h2>
+    <p class="movies__description">Miraculous evacuation of Allied soldiers from
+    Belgium, Britain, Canada, and France, who were cut off and surrounded by
+    the German army from the beaches and harbor of Dunkirk, France, during the
+    Battle of France in World War II.</p>
+    </div>`
+  };
+  const InterstellarBlurb = {
+  name: 'interstellar-blurb',
+  template: `<div>
+    <h2>Interstellar</h2>
+    <p class="movies__description">Interstellar chronicles the adventures of a
+    group of explorers who make use of a newly discovered wormhole to surpass
+    the limitations on human space travel and conquer the vast distances
+    involved in an interstellar voyage.</p>
+    </div>`
+  };
+  const TheDarkKnightRisesBlurb = {
+  name: 'the-dark-knight-rises-blurb',
+  template: `<div>
+    <h2>The Dark Knight Rises</h2>
+    <p class="movies__description">Batman encounters the mysterious Selina Kyle
+    and the villainous Bane, a new terrorist leader who overwhelms Gotham's
+    finest. The Dark Knight resurfaces to protect a city that has branded him
+   an enemy.</p>
+   </div>`
+  };
+
+
+const View = {
+  name: 'router-view',
+  template: `<component :is="currentView"></component>`,
+  data(){
+    return{
+      currentView: DunkirkBlurb
+    }
+  }
+};
+
 const App = {
   name: 'App',
   template: `<div id="app">
@@ -9,12 +63,11 @@ const App = {
       <a href="/interstellar">/interstellar</a>
       <a href="/the-dark-knight-rises">/the-dark-knight-rises</a>
 
-      <div>
-        <h2>Christopher Nolan Movies</h2>
-        <p>This is where the description of each movie will live!</p>
-      </div>
+      <router-view></router-view>
     </div>
-  </div>`
+  </div>`,
+  components: {
+    'router-view': View
+  }
 };
-
 export default App;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './app/app';
+import { router } from './app/app';
+
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 });
